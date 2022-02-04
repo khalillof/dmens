57c4f3248fd2a5f8888b048b727013f02cdccbde
K Tuban <tuban@hotmail.co.uk>
2022-02-04 14:50:37 +0000

    rename folders purets and purejs working app1

---
+++ purets/bin/config.ts
@@ -0,0 +1,17 @@
+//require('dotenv').config()
+
+export const config = {
+  secretKey: process.env.SECRET_KEY || '',
+  mongoUrl: {
+    'dev': process.env.DB_CONNECTION_DEV || '',
+    'local': process.env.DB_CONNECTION_LOCAL || '',
+    'prod': process.env.DB_CONNECTION_PROD || '',
+    'admin': process.env.DB_CONNECTION_ADMIN || '',
+    'cluster': process.env.DB_CONNECTION_CLUSTER || ''
+  },
+  facebook: {
+    'clientId': process.env.FACEBOOK_CLIENT_ID || '',
+    'clientSecret': process.env.FACEBOOK_CLIENT_SECRET || ''
+  }
+
+}
