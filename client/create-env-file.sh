a344beb504ef41e216ef8851086cd22c1c2f2670
K Tuban <tuban@hotmail.co.uk>
2022-08-20 23:32:29 +0100

    moved enviroment cfeation to sh file

--- client/create-env-file.sh
+++ client/create-env-file.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 touch .env
-echo PORT=${{ secrets.PORT }} >> .env
-echo NODE_ENV=${{ secrets.NODE_ENV }} >> .env
-echo ADMIN_EMAIL=${{ secrets.ADMIN_EMAIL }} >> .env
-echo ADMIN_USERNAME=${{ secrets.ADMIN_USERNAME }} >> .env
-echo ADMIN_PASSWORD=${{ secrets.ADMIN_USERNAME}} >> .env
-echo SECRET_KEY=${{ secrets.SECRET_KEY }} >> .env
-echo JWT_SECRET=${{ secrets.JWT_SECRET }} >> .env
-echo JWT_EXPIRATION=${{secrets.JWT_EXPIRATION }}  >> .env
-echo JWT_REFRESH_EXPIRATION=${{secrets.JWT_REFRESH_EXPIRATION }} >> .env
-echo ISSUER=${{secrets.ISSUER }} >> .env
-echo AUDIENCE=${{secrets.AUDIENCE }} >> .env
-echo GCP_MAP_API_KEY=${{secrets.GCP_MAP_API_KEY }} >> .env
-echo COSMOSDB_CON=${{secrets.COSMOSDB_CON }} >> .env
-echo DB_CONNECTION_CLUSTER=${{secrets.DB_CONNECTION_CLUSTER }} >> .env
-echo SCHEMA_DIR_PROD=${{secrets.SCHEMA_DIR_PROD }} >> .env
-echo IMAGES_UPLOAD_DIR_PROD=${{secrets.IMAGES_UPLOAD_DIR_PROD }} >> .env
-echo ORIGIN_PROD=${{secrets.ORIGIN_PROD }} >> .env
-echo CORES_DMAINS_PROD=${{secrets.CORES_DMAINS_PROD }} >> .env
-echo STATIC_URL_PROD=${{secrets.STATIC_URL_PROD }} >> .env
+echo PORT=$PORT  >> .env
+echo NODE_ENV=$NODE_ENV >> .env
+echo ADMIN_EMAIL=$ADMIN_EMAIL >> .env
+echo ADMIN_USERNAME=$ADMIN_USERNAME >> .env
+echo ADMIN_PASSWORD=$ADMIN_USERNAME >> .env
+echo SECRET_KEY=$SECRET_KEY >> .env
+echo JWT_SECRET=$JWT_SECRET >> .env
+echo JWT_EXPIRATION=$JWT_EXPIRATION   >> .env
+echo JWT_REFRESH_EXPIRATION=$JWT_REFRESH_EXPIRATION  >> .env
+echo ISSUER=$ISSUER  >> .env
+echo AUDIENCE=$AUDIENCE  >> .env
+echo GCP_MAP_API_KEY=$GCP_MAP_API_KEY  >> .env
+echo COSMOSDB_CON=$COSMOSDB_CON >> .env
+echo DB_CONNECTION_CLUSTER=$DB_CONNECTION_CLUSTER  >> .env
+echo SCHEMA_DIR_PROD=$SCHEMA_DIR_PROD  >> .env
+echo IMAGES_UPLOAD_DIR_PROD=$IMAGES_UPLOAD_DIR_PROD >> .env
+echo ORIGIN_PROD=$ORIGIN_PROD >> .env
+echo CORES_DMAINS_PROD=$CORES_DMAINS_PROD >> .env
+echo STATIC_URL_PROD=$STATIC_URL_PROD >> .env
 
 echo "printing the first two line from the .env file"
 head -2 ./.env
