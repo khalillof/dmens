109499562f14ddb741aeb65c533f3a6399d5d7b6
Khalil Tuban <tuban@hotmail.co.uk>
2023-10-14 17:19:55 +0100

    authenticate

--- src/routes/lib/account.routes.config.ts
+++ src/routes/lib/account.routes.config.ts
@@ -6,22 +6,22 @@ import { DefaultRoutesConfig } from './default.routes.config.js';
 export async function AccountRoutes(){
   return new DefaultRoutesConfig(new AccountController(),
      function (this: IDefaultRoutesConfig): void {
-        this.buidRoute(this.routeName+'/signup','post', 'signup',['checkLoginUserFields','authenticate'])
-        this.buidRoute(this.routeName+'/signin','post', 'signin',['checkLoginUserFields','authenticate'])
+        this.buidRoute(this.routeName+'/signup','post', 'signup',['checkLoginUserFields'])
+        this.buidRoute(this.routeName+'/signin','post', 'signin',['checkLoginUserFields'])
         this.buidRoute(this.routeName+'/logout','get', 'logout')
-        this.buidRoute(this.routeParam,'get','findById', ['isAuthenticated', 'validateCurrentUserOwnParamId']);
-        this.buidRoute(this.routeParam,'delete','remove', ['isAuthenticated','validateCurrentUserOwnParamId']);
-        this.buidRoute(this.routeParam,'put','put', ['isAuthenticated','validateCurrentUserOwnParamId']);
+        this.buidRoute(this.routeParam,'get','findById', ['validateCurrentUserOwnParamId','authenticate']);
+        this.buidRoute(this.routeParam,'delete','remove', ['validateCurrentUserOwnParamId','authenticate']);
+        this.buidRoute(this.routeParam,'put','put', ['validateCurrentUserOwnParamId','authenticate']);
         // get secrets form envirmoment variables
-        this.buidRoute(this.routeName+'/secure','get','secure', ['isAuthenticated','isAdmin']);
+        this.buidRoute(this.routeName+'/secure','get','secure', ['authenticate','isAdmin']);
         // all accounts
-        this.buidRoute(this.routeName,'list','list', ['isAuthenticated','isAdmin']);
+        this.buidRoute(this.routeName,'list','list', ['authenticate','isAdmin']);
 
         // get profile require query string eg ==>  /auth/profile?email=user@user.co
-        this.buidRoute(this.routeName,'get','profile', ['isAuthenticated','validateHasQueryEmailBelongToCurrentUser']);
+        this.buidRoute(this.routeName,'get','profile', ['authenticate','validateHasQueryEmailBelongToCurrentUser']);
 
-          this.buidRoute(this.routeName+'/search','list','search',['isAuthenticated','isAdmin']);// search
-          this.buidRoute(this.routeName+'/count','list','count', ['isAuthenticated','isAdmin']); // count
+          this.buidRoute(this.routeName+'/search','list','search',['authenticate','isAdmin']);// search
+          this.buidRoute(this.routeName+'/count','list','count', ['authenticate','isAdmin']); // count
           this.buidRoute(this.routeName+'/form','list','form'); // get form elements
           this.buidRoute(this.routeName+'/route','list','route'); // get form elements
 
