17a6a490843b5cbc3ff8287c6841ea14b7070690
Khalil Tuban <tuban@hotmail.co.uk>
2024-05-26 16:07:27 +0100

    fixed configs viewsData and authintication   ...!2

--- src/routes/lib/config.routes.config.ts
+++ src/routes/lib/config.routes.config.ts
@@ -8,11 +8,12 @@ export async function ConfigRoutes() {
     return new DefaultRoutesConfig(new ConfigController(),
         async function (this: IDefaultRoutesConfig) {
             await this.defaultClientRoutes()
-            await this.defaultRoutes()
+            //await this.defaultRoutes()
             await this.list(this.addPath('/routes'),'routes', ['authenticate', 'isAdmin'])
             await this.delete(this.addPath('/route/delete'), 'deleteRoute', ['authenticate', 'isAdmin'])
             await this.list(this.addPath('/forms'), 'forms') 
             await this.list(this.addPath('/viewsdata'), 'viewsData') 
+            await this.list()
             
 
         }
